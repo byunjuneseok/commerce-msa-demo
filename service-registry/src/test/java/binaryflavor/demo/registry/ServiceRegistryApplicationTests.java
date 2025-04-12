c0d4884e30acb7085a4d9ba7ae7a4868d7e2b37e
byunjuneseok <byunjuneseok@gmail.com>
2025-04-12 17:14:09 +0900

    MSD-2 Adds service registry module
    Bootstraps a new service registry module using Spring Cloud Netflix Eureka.
    
    Includes initial configuration, gradle setup, gitignore and necessary dependencies for
    service registration and discovery.

---
+++ service-registry/src/test/java/binaryflavor/demo/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,15 @@
+package binaryflavor.demo.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
