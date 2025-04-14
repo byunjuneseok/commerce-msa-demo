9c3b142aa65aca35210eb50113dbf4cdfcaea1e4
byunjuneseok <byunjuneseok@gmail.com>
2025-04-14 19:50:06 +0900

    MSD-14 Updates image tags for inventory and order APIs
    Updates the image tags for the inventory-api and order-api deployments to use the latest build.

--- inventory-api/src/main/java/binaryflavor/demo/inventory/config/RedisConfig.java
+++ inventory-api/src/main/java/binaryflavor/demo/inventory/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package binaryflavor.demo.inventory.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -8,10 +9,16 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig
 {
+  @Value("${spring.data.redis.host}")
+  private String host;
+
+  @Value("${spring.data.redis.port}")
+  private int port;
+
   @Bean
   public LettuceConnectionFactory redisConnectionFactory()
   {
-    return new LettuceConnectionFactory();
+    return new LettuceConnectionFactory(host, port);
   }
 
   @Bean
